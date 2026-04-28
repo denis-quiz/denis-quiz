0b8bf29b202af60b681793c5d72ee6a045b5e5f4
v1n3l0v3r <denisperekhodko@gmail.com>
2026-04-28 16:45:18 +0200

    Integrated neondDb and drizzle with backend. Added tables users and quizzes(without colums cause I'm not sure on their structure yet)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
+import { sql } from 'bun';
+import {drizzle, NeonHttpDatabase} from 'drizzle-orm/neon-http';
 
 const app = new Hono()
+const db = drizzle(process.env.DATABASE_URL!);
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+async function getPgVersion() {
+  const result = await sql`SELECT version()`;
+  console.log(result[0]);
+}
+
+getPgVersion();
 
 export default app
