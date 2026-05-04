2cd6a56357cfa49133243fe287d63898c45e81e0
v1n3l0v3r <denisperekhodko@gmail.com>
2026-05-04 13:07:17 +0200

    Setup of drizzle orm and neon db connection

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,27 +1,20 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { drizzle, NeonHttpDatabase } from "drizzle-orm/neon-http";
-
+import { neon } from "@neondatabase/serverless";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
 const app = new Hono();
-const db = drizzle(process.env.DATABASE_URL!);
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+const db = drizzle(process.env.DATABASE_URL!);
+app.get("/", async (c) => {
+  try {
+    const sql = neon(process.env.DATABASE_URL!);
+    const response = await sql`SELECT version()`;
+    return c.json({ version: response[0]?.version });
+  } catch (error) {
+    console.error("Database query failed:", error);
+    return c.text("Failed to connect to database", 500);
+  }
 });
 
-async function getPgVersion() {
-  const result = await sql`SELECT version()`;
-  console.log(result[0]);
-}
-
-getPgVersion();
-
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  },
-);
+serve(app);
