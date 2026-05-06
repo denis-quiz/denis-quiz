30bc0f842eadd6d570a341eeff6b408b260b6a5c
v1n3l0v3r <denisperekhodko@gmail.com>
2026-05-06 10:52:08 +0200

    modified git ignore in orsder to prevent commiting cache of trubo and next and also added a .env.example

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,10 +4,12 @@ import "dotenv/config";
 import { db } from "./db/db.js";
 import { healthRoutes } from "./health/health.routes.js";
 import { quizzesRoutes } from "./quizzes/quizzes.routes.js";
+import { authenticationRoutes } from "./auth/auth.routes.js";
 
 const app = new Hono();
 
 healthRoutes(app, db);
 quizzesRoutes(app, db);
+authenticationRoutes(app);
 
 serve(app);
