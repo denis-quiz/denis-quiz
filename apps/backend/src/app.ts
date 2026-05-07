e44413117eb268ddfcb9d6372461c564460710a0
v1n3l0v3r <denisperekhodko@gmail.com>
2026-05-07 13:06:34 +0200

    Removed db-debug endpoint

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -19,18 +19,6 @@ app.use(
   }),
 );
 
-app.get("/debug-db", async (c) => {
-    const dbName = await db.execute(sql`select current_database();`);
-
-    const tables = await db.execute(sql`
-    select tablename
-    from pg_tables
-    where schemaname = 'public';
-  `);
-
-    return c.json({ dbName, tables });
-});
-
 healthRoutes(app, db);
 quizzesRoutes(app, db);
 authenticationRoutes(app);
