f90b3bc4e5fbb08ec9edae583835fc09c735ddfa
v1n3l0v3r <denisperekhodko@gmail.com>
2026-05-14 09:24:05 +0200

    Enchanced error handling and added toast in order to improve ui

---
+++ apps/frontend/lib/toasts/sucess.ts
@@ -0,0 +1,31 @@
+import { showToast } from "nextjs-toast-notify";
+
+export function successToast(
+  message: string,
+  duration = 4000,
+  position: ToastPosition = "top-center",
+  transition: ToastTransition = "bounceIn",
+) {
+  showToast.success(message, {
+    duration,
+    position,
+    transition,
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.75"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M20 6 9 17l-5-5"/>
+      </svg>
+    `,
+    sound: true,
+    progress: true,
+  });
+}
